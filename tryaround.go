27cf85ca6e24f4003700e26c225a78f7fb1070ea
Paul Seyfert <pseyfert.mathphys@gmail.com>
2018-11-09 23:20:13 +0100

    drafted project support

--- tryaround.go
+++ tryaround.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	_ "flag"
+	"flag"
 	"log"
 	"os"
 	"path/filepath"
@@ -10,6 +10,7 @@ import (
 	"github.com/alecthomas/participle"
 	"github.com/alecthomas/participle/lexer"
 	"github.com/alecthomas/participle/lexer/ebnf"
+	"github.com/pseyfert/compilecommands_to_compilerexplorer/cc2ce4lhcb"
 )
 
 var replacementvars = map[string]string{
@@ -69,21 +70,34 @@ func parse(fname string) (*ListsFile, error) {
 }
 
 func main() {
-	platformconfigpath := "/cvmfs/lhcbdev.cern.ch/nightlies/lhcb-head/latest/REC/REC_HEAD/InstallArea/x86_64+avx2+fma-centos7-gcc7-opt/cmake/RecPlatformConfig.cmake"
+	var p cc2ce4lhcb.Project
+	flag.StringVar(&p.Slot, "slot", "lhcb-head", "nightlies slot (i.e. directory in /cvmfs/lhcbdev.cern.ch/nightlies/)")
+	flag.StringVar(&p.Day, "day", "Today", "day/buildID (i.e. subdirectory, such as 'Today', 'Mon', or '2032')")
+	flag.StringVar(&p.Project, "project", "Rec", "project (such as Rec, Brunel, LHCb, Lbcom)")
+	flag.StringVar(&p.Version, "version", "HEAD", "version (i.e. the stuff after the underscore like HEAD or 2016-patches)")
+	flag.StringVar(&cc2ce4lhcb.Cmtconfig, "cmtconfig", "x86_64+avx2+fma-centos7-gcc7-opt", "platform, like x86_64+avx2+fma-centos7-gcc7-opt or x86_64-centos7-gcc7-opt")
+	flag.StringVar(&cc2ce4lhcb.Nightlyroot, "nightly-base", "/cvmfs/lhcbdev.cern.ch/nightlies/", "add the specified directory to the nightly builds search path")
+	flag.Parse()
+	p.Project = strings.ToUpper(p.Project)
+	mixedcasename := p.Project[0:1] + strings.ToLower(p.Project[1:len(p.Project)])
+
+	platformconfigpath := filepath.Join(cc2ce4lhcb.Installarea(p), "cmake", mixedcasename+"PlatformConfig.cmake")
+	thislibdir := filepath.Join(cc2ce4lhcb.Installarea(p), "lib")
+	upperCaseL[thislibdir] = true
 	platformconfig, err := parse(platformconfigpath)
 	if err != nil {
 		log.Printf("Couldn't open project config: %v", err)
 		os.Exit(7)
 	}
 	for _, funccall := range platformconfig.Functions {
-		if funccall.FunctionName == "set(" && funccall.Fargs[0] == "Rec_LINKER_LIBRARIES" {
+		if funccall.FunctionName == "set(" && funccall.Fargs[0] == mixedcasename+"_LINKER_LIBRARIES" {
 			for _, linklib := range strings.Split(funccall.Fargs[1], ";") {
 				linkerlibs[linklib] = false
 			}
 		}
 	}
 
-	exportfiles, err := filepath.Glob("/cvmfs/lhcbdev.cern.ch/nightlies/lhcb-head/latest/REC/REC_HEAD/InstallArea/x86_64+avx2+fma-centos7-gcc7-opt/cmake/*Export.cmake")
+	exportfiles, err := filepath.Glob(filepath.Join(cc2ce4lhcb.Installarea(p), "cmake", "*Export.cmake"))
 	if err != nil {
 		log.Printf("couldn't glob export files: %v", err)
 		os.Exit(9)
@@ -138,6 +152,8 @@ func main() {
 	for l, r := range linkerlibs {
 		if !r {
 			log.Printf("WARNING: couldn't resolve link deps for %s", l)
+		} else {
+			lowerCaseL[l] = true
 		}
 	}
 	for l, _ := range all_libs {
@@ -151,7 +167,9 @@ func main() {
 			upperCaseL[dir] = true
 			lowerCaseL[validateme] = true
 		} else {
-			targetLs[l] = true
+			if _, found := linkerlibs[l]; !found {
+				targetLs[l] = true
+			}
 		}
 	}
 	for l, _ := range upperCaseL {
