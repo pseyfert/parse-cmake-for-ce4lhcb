612e5a587124f63530358f8b3543ee5ad8407727
Paul Seyfert <pseyfert.mathphys@gmail.com>
2019-09-18 12:51:03 +0200

    fixup config name collision

--- cc4ce_extensions/utils.go
+++ cc4ce_extensions/utils.go
@@ -23,7 +23,7 @@ type CompilerConfig struct {
 func CompilerAndOptions(p cc2ce4lhcb.Project, nightlyroot, cmtconfig string) (CompilerConfig, error) {
 	retval, err := CompilerAndOptionsFromJsonByFilename(cc2ce4lhcb.Installarea(p))
 	retval.Name = fmt.Sprintf("%s-%s", p.Project, cmtconfig)
-	retval.ConfName = p.CE_config_name()
+	retval.ConfName = strings.ToLower(retval.Name)
 	return retval, err
 }
 
